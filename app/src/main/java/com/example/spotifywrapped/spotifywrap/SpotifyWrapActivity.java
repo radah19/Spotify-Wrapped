25eaf8f14eb266c40d69864c71666cc2e8168fad
Anuj Adulla <anuj.adulla@gmail.com>
2024-03-12 11:45:04 -0400

    Scaffolding for Spotify Wrap ViewPager Added

---
+++ app/src/main/java/com/example/spotifywrapped/spotifywrap/SpotifyWrapActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotifywrapped.spotifywrappedview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spotifywrapped.R;
+
+public class SpotifyWrapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_wrap);
+    }
+}
---
+++ app/src/main/java/com/example/spotifywrapped/spotifywrap/SpotifyWrapAdapter.java
@@ -0,0 +1,2 @@
+package com.example.spotifywrapped.spotifywrap;public class SpotifyWrapAdapter {
+}
