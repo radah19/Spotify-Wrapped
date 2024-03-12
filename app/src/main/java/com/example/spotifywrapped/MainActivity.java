613e322457dee274c50a7a747039a25d209b771c
Ethan Lee <efamlee4@gmail.com>
2024-03-12 01:21:11 -0400

    Added create account functionality and related methods

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.spotifywrapped;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button register, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        register.setOnClickListener((View v) -> {
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        });
     }
 }
