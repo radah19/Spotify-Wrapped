df7ba23cc23a2f39302ecd6fbf3dd5601d246279
Anuj Adulla <anuj.adulla@gmail.com>
2024-03-15 21:31:00 -0400

    Navbar Implemented

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -11,8 +11,6 @@ import com.example.spotifywrapped.accountscreen.LoginActivity;
 import com.example.spotifywrapped.spotifywrappedlist.SpotifyWrappedListActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
