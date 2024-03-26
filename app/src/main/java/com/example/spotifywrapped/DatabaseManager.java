78ede96562bf4466284811ac5cefd4207ab3f0a8
Ethan Lee <efamlee4@gmail.com>
2024-03-26 13:24:49 -0400

    Adding login and create account functionality with database.

--- app/src/main/java/com/example/spotifywrapped/DatabaseManager.java
+++ app/src/main/java/com/example/spotifywrapped/DatabaseManager.java
@@ -1,13 +1,17 @@
+
 package com.example.spotifywrapped;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.common.PackageVerificationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,9 +20,16 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.checkerframework.checker.units.qual.A;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
 public class DatabaseManager {
 
     private static FirebaseAuth mAuth;
+    private static FirebaseDatabase rootNode;
+    private static DatabaseReference reference;
 
     public DatabaseManager() {
     } // DatabaseManager
@@ -47,5 +58,51 @@ public class DatabaseManager {
             return false;
         } // if
     } // isLoggedin
+
+    public static void inputVerification(String email, String password, Activity activity) {
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(activity, "Empty credentials!", Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 6){
+            Toast.makeText(activity, "Password too short!", Toast.LENGTH_SHORT).show();
+        } else {
+            createNewAccount(email, password, activity);
+        } // if
+    } // inputVerification
+
+    public void loginUser(String email, String password, Activity activity) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(activity, "Login Successful!", Toast.LENGTH_SHORT).show();
+                // need to switch activities and also call finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(activity, "Login Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    } // loginUser
+
+    public void logOut() {
+        FirebaseAuth.getInstance().signOut();
+    } // logOut
+
+    public static void initializeUserTree() {
+        // reference = rootNode.getReference("Users");
+    } // initializeUserTree
+
+    public static void addUser(String username, String password, ArrayList<String> friendsList, int spotifyUserID) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("Password", password);
+        map.put("Friends List", friendsList);
+        map.put("SpotifyUserID", spotifyUserID);
+        FirebaseDatabase.getInstance().getReference().child("Users").child(username).setValue(map);
+    } // addUser
+
+    public static void retrieveUser() {
+
+    } // retrieveUser
+
 
 } // DatabaseManager
--- app/src/main/java/com/example/spotifywrapped/accountscreen/CreateAccountActivity.java
+++ app/src/main/java/com/example/spotifywrapped/accountscreen/CreateAccountActivity.java
@@ -3,14 +3,37 @@ package com.example.spotifywrapped.accountscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.spotifywrapped.DatabaseManager;
 import com.example.spotifywrapped.R;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    public EditText email, password, username;
+    public Button createAccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        email = findViewById(R.id.email_input);
+        username = findViewById(R.id.username_input);
+        password = findViewById(R.id.password_input);
+        createAccount = findViewById(R.id.login_button);
+
+        String email_input = email.getText().toString();
+        String username_input = username.getText().toString();
+        String password_input = password.toString();
+
+
+        createAccount.setOnClickListener((View v) -> {
+            DatabaseManager.setFirebaseAuth();
+            DatabaseManager.createNewAccount(email_input, password_input, CreateAccountActivity.this);
+        });
     }
 }
