70d694bacb71af4d511a13471781e626cd46574c
Your Name <your@email.com>
2024-04-02 20:14:01 -0400

    settingsAdded

---
+++ app/src/main/java/com/example/spotifywrapped/settings/Settings.java
@@ -0,0 +1,47 @@
+package com.example.spotifywrapped.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.spotifywrapped.DatabaseManager;
+import com.example.spotifywrapped.R;
+import com.example.spotifywrapped.accountscreen.CreateAccountActivity;
+import com.example.spotifywrapped.accountscreen.LoginActivity;
+import com.example.spotifywrapped.navbar.NavbarClass;
+
+public class Settings extends AppCompatActivity {
+
+    public EditText oldPassword, newPassword;
+    public Button updatePassword_btn;
+    public Button logOut_btn;
+    public Button clearAccountData_btn;
+    public Button deleteAccount_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        //update password method
+        NavbarClass.initializeNavbar(this);
+
+        oldPassword = findViewById(R.id.oldPassword_input);
+        newPassword = findViewById(R.id.newPassword_input);
+
+        String oldPassword_input = oldPassword.getText().toString();
+        String newPassword_input = newPassword.getText().toString();
+
+    }
+
+    //log out method
+
+    //delete account method
+
+    //clear data method
+}
